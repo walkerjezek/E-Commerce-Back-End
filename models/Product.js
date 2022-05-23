52c2ea405a81a724bb979bde8b6b8246f69386ea
walkerj <wpjezek@gmail.com>
2022-05-23 14:40:08 -0600

    back from trip. building out columns

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,34 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    },
+    product_name: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isNumeric: true,
+      }
+    },
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -4,13 +4,41 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+// Ref: 13-01-23 for how to do this.
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'categoryId'
+});
+
 
 // Categories have many Products
+Product.hasMany(Product, {
+  foreignKey: 'categoryId'
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'productId',
+    through: {
+      model: ProductTag,
+      unique: false
+    }
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'tagId',
+    through: {
+      model: ProductTag,
+      unique: false
+    }
+});
+
+
 
 module.exports = {
   Product,
